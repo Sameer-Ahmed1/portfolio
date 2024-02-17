c8848c29976e3de5c7a1100b523252b54973f0fb
Sameer Ahmed <sameernadeemi1111@gmail.com>
2024-02-18 01:33:26 +0530

    added projects

---
+++ projects/projects.js
@@ -0,0 +1,28 @@
+// projects.js
+const projects = [
+  {
+    title: "PhoneBook",
+    description:
+      "A phonebook with add, update, delete features and user authentication using bcrypt and jsonwebtoken.",
+    image: "/assets/projects/phonebook.png",
+    deploymentLink: "https://phonebook-hou8.onrender.com/",
+    githubLink: "https://github.com/Sameer-Ahmed1/phonebook",
+  },
+  {
+    title: "Coeditor",
+    description:
+      "A realtime collaborative co editor built using websockets with room based collaboration and chat feature.",
+    image: "/assets/projects/coeditor.png",
+    deploymentLink: "https://coeditor.msameerahmed.com/",
+    githubLink: "https://github.com/Sameer-Ahmed1/Coeditor",
+  },
+  {
+    title: "EtchASketch",
+    description:
+      " A JavaScript pixel art tool enabling users to draw on a hover-sensitive grid, produce  pixel art.",
+    image: "/assets/projects/sketch.png",
+    deploymentLink: "https://sameer-ahmed1.github.io/etch-a-sketch/",
+    githubLink: "https://github.com/Sameer-Ahmed1/etch-a-sketch",
+  },
+  // Add more projects as needed
+];
